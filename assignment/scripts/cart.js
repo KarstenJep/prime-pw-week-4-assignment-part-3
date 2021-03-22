6077baf42e6b889dbfb7784536c7d5c0d838be28
Karsten Jepsen <Kjepsen86@gmail.com>
2021-03-21 20:38:08 -0500

    w4p3Completed Required & Stretch

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,10 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-// Create a fuction addItem
 let basket = [];
+// Create a fuction addItem
 function addItem(item){
-  console.log('Using "addItem" function;', item);
+  console.log('In "addItem" function;', item);// Testing function
   basket.push(item);
   return true
 }
@@ -15,21 +15,21 @@ addItem('beer'); // Calling function
 console.log('Test - should be 3 items in basket', basket); // Testing function
 
 // Create a function called listItems
-function listItems( ){
-console.log('Using "listItems" function;');
-  for (let object of basket) {
-    console.log(object);
+function listItems( array ){
+console.log('In "listItems" function;', array); // Testing function
+  for (let item of basket) {
+    console.log(item);
   }
 }
-listItems(); // Calling function
+listItems(basket); // Calling & Testing function
 console.log('Test - should still be 3 items in basket', basket); // Testing function
 
-// Create a function called empty
-function empty( ){
-console.log('Using "empty" function;');
-  basket.length = 0;
+// // Create a function called empty
+function empty(array){
+console.log('In "empty" function;', array); // Testing function
+  array.length = 0;
 }
-empty(); // Calling function
+empty(basket); // Calling function
 console.log('Test - basket should be empty', basket); // Testing function
 
 // SSTTRRREEEEECCHHH GOALS
@@ -37,7 +37,7 @@ console.log('Test - basket should be empty', basket); // Testing function
 const maxItems = 5;
 
 // 2.
-function isFull( ){
+function isFull(){
   if (maxItems > basket.length) {
     return false;
   } return true;
@@ -47,7 +47,7 @@ console.log('Test - should say false', isFull()); // Testing function
 // 3.
 function addItemUpdated(item){
   if (isFull() === false) {
-    console.log('Using "addItem" function;', item);
+    console.log('Using "addItem" function;', item); // Testing function
     basket.push(item);
     return true;
   } return false;
@@ -57,25 +57,22 @@ addItemUpdated('Item1'); // Calling function
 addItemUpdated('Item2'); // Calling function
 addItemUpdated('Item3'); // Calling function
 addItemUpdated('Item4'); // Calling function
-console.log('Test - should say true', addItemUpdated()); // Testing function
-console.log('Test - should be 4 items', basket); // Testing function
 addItemUpdated('Item5'); // Calling function
 addItemUpdated('Item6'); // Calling function
+console.log('Test - should say true', addItemUpdated()); // Testing function
+console.log('Test - should be 4 items', basket); // Testing function
 console.log('Test - should be say false', addItemUpdated()); // Testing function
 console.log('Test - should be 5 items', basket); // Testing function
-// ** After using the addItemUpdated function to add 4 items, a 'undefined' item was
-// added the the basket as well. I am not sure why that's happening...**
+
 
 // 4.
-let pets = ['dog', 'cat', 'parrot', 'goldfish', 'iguana'];
-function removeItem( item ){
-  console.log('In q4 function', pets);
-  for (let i=0; i <pets.length; i++){
-    console.log(item);
-    if (item = pets.indexOf(i)){
-      console.log(pets, item);
-      return pets.splice([i], 1);
-    } return null;
-  }
+function removeItem(item){
+  console.log('In "removeItem" function', item);
+  for (let i=0; i<basket.length; i++){
+    if (item === basket.indexOf(i), 1) {
+      basket.splice(basket.indexOf(item), 1);
+    } return item;
+  } return null;
 }
-console.log('Test - ', removeItem('parrot'));
+removeItem('Item4')
+console.log('Test - should remove Item4', basket);
